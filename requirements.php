16bac0b1c8e7fc8f1e82ff22376ded987d82b21f
vova07 <bazillio07@yandex.ru>
2014-01-25 10:07:44 +0200

    Мелкие правки.

--- requirements.php
+++ requirements.php
@@ -11,12 +11,12 @@
  */
 
 // you may need to adjust this path to the correct Yii framework path
-$frameworkPath = dirname(__FILE__) . '/vendor/yiisoft/yii2/yii';
+$frameworkPath = dirname(__FILE__) . '/vendor/yiisoft/yii2';
 
 if (!is_dir($frameworkPath)) {
 	echo '<h1>Error</h1>';
 	echo '<p><strong>The path to yii framework seems to be incorrect.</strong></p>';
-	echo '<p>You need to install Yii framework via composer or adjust the framework path in file <abbr title="' . __FILE__ . '">' . basename(__FILE__) .'</abbr>.</p>';
+	echo '<p>You need to install Yii framework via composer or adjust the framework path in file <abbr title="' . __FILE__ . '">' . basename(__FILE__) . '</abbr>.</p>';
 	echo '<p>Please refer to the <abbr title="' . dirname(__FILE__) . '/README.md">README</abbr> on how to install Yii.</p>';
 }
 
@@ -48,6 +48,13 @@ $requirements = array(
 		'by' => 'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>',
 		'memo' => 'Required for MySQL database.',
 	),
+	array(
+		'name' => 'PDO PostgreSQL extension',
+		'mandatory' => false,
+		'condition' => extension_loaded('pdo_pgsql'),
+		'by' => 'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>',
+		'memo' => 'Required for PostgreSQL database.',
+	),
 	// Cache :
 	array(
 		'name' => 'Memcache extension',
@@ -59,7 +66,7 @@ $requirements = array(
 	array(
 		'name' => 'APC extension',
 		'mandatory' => false,
-		'condition' => extension_loaded('apc') || extension_loaded('apc'),
+		'condition' => extension_loaded('apc'),
 		'by' => '<a href="http://www.yiiframework.com/doc/api/CApcCache">CApcCache</a>',
 	),
 	// Additional PHP extensions :
