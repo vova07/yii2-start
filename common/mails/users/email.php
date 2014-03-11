eb69052863259230fd9ecf4e6ce79a5b04c02f13
Tom910 <Tom910@mail.ru>
2014-03-10 23:47:06 -0700

    Не приходил ключ активации

--- common/mails/users/email.php
+++ common/mails/users/email.php
@@ -8,7 +8,7 @@
  
 use yii\helpers\Html;
 
-$url = Yii::$app->urlManager->createAbsoluteUrl('users/default/email', ['key' => $key, 'email' => $email]);
+$url = Yii::$app->urlManager->createAbsoluteUrl(['users/default/email', 'key' => $key, 'email' => $email]);
 ?>
 <h3>Здравствуйте!</h3>
 <p>Для того чтобы подтвердить смену email адреса вы должны перейти по ссылке: <?= Html::a(Html::encode($url), $url) ?>.</p>
