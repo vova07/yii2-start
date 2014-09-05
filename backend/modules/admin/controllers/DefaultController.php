4b513455e88264253535b40ec0c45b45b707865b
vova07 <bazillio07@yandex.ru>
2014-09-05 13:48:12 +0300

    Fix for issue #67

--- backend/modules/admin/controllers/DefaultController.php
+++ backend/modules/admin/controllers/DefaultController.php
@@ -19,7 +19,7 @@ class DefaultController extends Controller
         $behaviors['access']['rules'][] = [
             'allow' => true,
             'actions' => ['error'],
-            'roles' => ['?', '@']
+            'roles' => ['@']
         ];
 
         return $behaviors;
